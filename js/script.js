bdec3c4619842ec47c126fa68af4d4659ce06e90
karthick-selvam <karthick.e1595@gmail.com>
2023-02-07 19:25:27 +0530

    WIP

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const text = document.getElementsByClassName("text");
+// const texts = ["VISION", "CLARITY", "INSIGHT"];
+// let i = 0;
+
+// setInterval(() => {
+// 	text[0].innerHTML = texts[i];
+// 	i = (i + 1) % texts.length;
+// }, 3000);
+
+// STYLESHEETS
+const indexStyleSS = document.getElementById("index-styles");
+const aiSolutionsSS = document.getElementById("ai-solutions-styles");
+const productsSS = document.getElementById("products-styles");
+
+const navList = document.querySelectorAll(".main-nav-link");
--- script.js
+++
@@ -1,8 +0,0 @@
-// const text = document.getElementsByClassName("text");
-// const texts = ["VISION", "CLARITY", "INSIGHT"];
-// let i = 0;
-
-// setInterval(() => {
-// 	text[0].innerHTML = texts[i];
-// 	i = (i + 1) % texts.length;
-// }, 3000);
