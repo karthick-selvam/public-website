c4f32c0f4549db8ce4f11ecc8b570f7c4d2844f8
karthick-selvam <karthick.e1595@gmail.com>
2023-02-08 18:20:22 +0530

    Added menu and modal

--- js/script.js
+++ js/script.js
@@ -1,15 +1,55 @@
-// const text = document.getElementsByClassName("text");
-// const texts = ["VISION", "CLARITY", "INSIGHT"];
-// let i = 0;
+var submenutrigger = document.querySelectorAll(".main-nav-list > li");
+var dropBackground = document.querySelector(".dropdownBackground");
+var nav = document.querySelector(".main-nav");
 
-// setInterval(() => {
-// 	text[0].innerHTML = texts[i];
-// 	i = (i + 1) % texts.length;
-// }, 3000);
+/*activate submenu*/
+function activeSub() {
+	var dropdown = this.querySelector(".dropdown");
+	if (dropdown) {
+		this.classList.add("trigger-enter");
+		setTimeout(
+			() =>
+				this.classList.contains("trigger-enter") &&
+				this.classList.add("trigger-enter-active"),
+			100
+		);
+		dropBackground.classList.add("open");
 
-// STYLESHEETS
-const indexStyleSS = document.getElementById("index-styles");
-const aiSolutionsSS = document.getElementById("ai-solutions-styles");
-const productsSS = document.getElementById("products-styles");
+		/*get position on mouseover*/
+		// console.log(dropdown);
 
-const navList = document.querySelectorAll(".main-nav-link");
+		var dropdownPos = dropdown.getBoundingClientRect();
+		// console.log(dropdownPos);
+		var navPos = nav.getBoundingClientRect();
+		// console.log(navPos);
+
+		var pos = {
+			height: dropdownPos.height,
+			width: dropdownPos.width,
+			top: dropdownPos.top - navPos.top,
+			left: dropdownPos.left - navPos.left,
+		};
+
+		/*set position of the arrow and submenu on mouseover*/
+		dropBackground.style.setProperty("width", `${pos.width}px`);
+		dropBackground.style.setProperty("height", `${pos.height}px`);
+		dropBackground.style.setProperty(
+			"transform",
+			`translate(${pos.left}px, ${pos.top}px)`
+		);
+	}
+}
+
+/*deactivate submenu*/
+function deactiveSub() {
+	this.classList.remove("trigger-enter", "trigger-enter-active");
+	dropBackground.classList.remove("open");
+}
+
+/*mouse events*/
+submenutrigger.forEach((trigger) =>
+	trigger.addEventListener("mouseenter", activeSub)
+);
+submenutrigger.forEach((trigger) =>
+	trigger.addEventListener("mouseleave", deactiveSub)
+);
